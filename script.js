58e544c446b52f2e4da146c0d3cafe385de9afdb
hichaaamb <hichambidoud@gmail.com>
2023-01-20 23:33:57 +0100

    Completing the js file, but was not the final version as i'll come back to create a popup announcing the final result and asking the player whether to replay or not

--- script.js
+++ script.js
@@ -1,29 +1,118 @@
-let choices = ["rock", "paper", "scissors"];
+let computerSelection = "";
+let playerSelection = "";
+let playerScoreValue = 0;
+let computerScoreValue = 0;
 
-function getComputerChoice() {
-  let randomIndex = Math.floor(Math.random() * 3);
-  computerSelection = choices[randomIndex];
-  return computerSelection;
+choices = ["✊", "✋", "✌️"];
+function getComputerSelection() {
+  computerSelection = choices[Math.floor(Math.random() * 3)];
 }
+
 function paper() {
-  playerSelection = "paper";
-}
-function scissors() {
-  playerSelection = "scissors";
+  playerSelection = "✋";
+  document.getElementById("player-emoji").textContent = playerSelection;
+  getComputerSelection();
+  document.getElementById("computer-emoji").textContent = computerSelection;
+  playRound(playerSelection, computerSelection);
 }
 function rock() {
-  playerSelection = "rock";
+  playerSelection = "✊";
+  document.getElementById("player-emoji").textContent = playerSelection;
+  getComputerSelection();
+  document.getElementById("computer-emoji").textContent = computerSelection;
+  playRound(playerSelection, computerSelection);
 }
-function playRound(computerSelection, playerSelection) {
-  if (
-    (computerSelection == "paper" && playerSelection == "rock") ||
-    (computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "scissors" && playerSelection == "paper")
-  ) {
-    alert("You lose!!");
-  } else if (computerSelection == playerSelection) {
-    alert("It's a tie!");
+function scissors() {
+  playerSelection = "✌️";
+  document.getElementById("player-emoji").textContent = playerSelection;
+  getComputerSelection();
+  document.getElementById("computer-emoji").textContent = computerSelection;
+  playRound(playerSelection, computerSelection);
+}
+function playRound(playerSelection, computerSelection) {
+  if (playerScoreValue == 5 && computerScoreValue < playerScoreValue) {
+    document.getElementById("title").textContent = "YOU'VE WON THE GAME !";
+    document.getElementById("title").style.color = "green";
+    reset();
   } else {
-    alert("You win !");
+    if (playerSelection == "✋" && computerSelection == "✌️") {
+      document.getElementById("status").textContent = "You lost!";
+      document.getElementById("description").textContent =
+        "Paper is beaten by scissors";
+
+      computerScoreValue += 1;
+      document.getElementById("player-score-value").textContent =
+        playerScoreValue;
+      document.getElementById("computer-score-value").textContent =
+        computerScoreValue;
+    }
+    if (playerSelection == "✊" && computerSelection == "✋") {
+      document.getElementById("status").textContent = "You lost!";
+      document.getElementById("description").textContent =
+        "Rock is beaten by paper";
+
+      computerScoreValue += 1;
+      document.getElementById("player-score-value").textContent =
+        playerScoreValue;
+      document.getElementById("computer-score-value").textContent =
+        computerScoreValue;
+    }
+    if (playerSelection == "✌️" && computerSelection == "✊") {
+      document.getElementById("status").textContent = "You lost!";
+      document.getElementById("description").textContent =
+        "Scissors is beaten by rock";
+
+      computerScoreValue += 1;
+      document.getElementById("player-score-value").textContent =
+        playerScoreValue;
+      document.getElementById("computer-score-value").textContent =
+        computerScoreValue;
+    }
+    if (playerSelection == "✌️" && computerSelection == "✌️") {
+      document.getElementById("status").textContent = "It's a tie!";
+      document.getElementById("description").textContent =
+        "Scissors ties with scissors";
+      document.getElementById("player-score-value").textContent =
+        playerScoreValue;
+      document.getElementById("computer-score-value").textContent =
+        computerScoreValue;
+    }
+    if (playerSelection == "✊" && computerSelection == "✊") {
+      document.getElementById("status").textContent = "It's a tie!";
+      document.getElementById("description").textContent =
+        "Rock ties with rock";
+      document.getElementById("player-score-value").textContent =
+        playerScoreValue;
+      document.getElementById("computer-score-value").textContent =
+        computerScoreValue;
+    }
+    if (playerSelection == "✋" && computerSelection == "✋") {
+      document.getElementById("status").textContent = "It's a tie!";
+      document.getElementById("description").textContent =
+        "Paper ties with paper";
+      document.getElementById("player-score-value").textContent =
+        playerScoreValue;
+      document.getElementById("computer-score-value").textContent =
+        computerScoreValue;
+    }
+    if (playerSelection == "✋" && computerSelection == "✊") {
+      document.getElementById("status").textContent = "You win!";
+      document.getElementById("description").textContent = "Paper beats rock";
+      playerScoreValue += 1;
+      document.getElementById("player-score-value").textContent =
+        playerScoreValue;
+      document.getElementById("computer-score-value").textContent =
+        computerScoreValue;
+    }
+    if (playerSelection == "✊" && computerSelection == "✌️") {
+      document.getElementById("status").textContent = "You win!";
+      document.getElementById("description").textContent =
+        "Rock beats scissors";
+      playerScoreValue += 1;
+      document.getElementById("player-score-value").textContent =
+        playerScoreValue;
+      document.getElementById("computer-score-value").textContent =
+        computerScoreValue;
+    }
   }
 }
